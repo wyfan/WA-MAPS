35b3ebcaa92ec697804b4784d72a2bf4f0d14988
WY <moonunique@gmail.com>
2026-01-06 17:24:25 +0800

    change file name
    change file name

--- tenkings/R1/TK-R1.tsx
+++ tenkings/R1/TK-R1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="十王-R1" tilewidth="1342" tileheight="918" tilecount="1" columns="1">
- <image source="../../../專案製作/地圖/1.入口第一幕/說岳開篇-尺寸調整-佩純20250508-v3.png" width="1342" height="918"/>
+<tileset version="1.10" tiledversion="1.11.2" name="TK-R1" tilewidth="1342" tileheight="918" tilecount="1" columns="1">
+ <image source="TK-R1-20250508-v3.png" width="1342" height="918"/>
 </tileset>
