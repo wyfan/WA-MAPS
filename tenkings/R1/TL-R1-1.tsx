3ffb3fac34e951296b5d747ee1faab890ecbb0aa
WY <moonunique@gmail.com>
2026-01-12 17:04:14 +0800

    add new map to try
    add new map to try

---
+++ tenkings/R1/TL-R1-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TK-R1-1" tilewidth="32" tileheight="32" tilecount="1148" columns="41">
+ <image source="TK-R1-20250508-v3.png" width="1342" height="918"/>
+</tileset>
