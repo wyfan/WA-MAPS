4e45514043cd227cfd9ee64381cd3f958c0e8718
WY <moonunique@gmail.com>
2026-01-26 17:03:44 +0800

    20260126 - 重新embed 第二個tileset檔案看看
    20260126 - 重新embed 第二個tileset檔案看看

---
+++ tenkings/R1/BaseChip_pipo.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="../Tiled素材/Pipoya RPG Tileset 32x32/[Base]BaseChip_pipo.png" width="256" height="4256"/>
+ <tile id="453">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
